407d036527a69978629e3f481d82adde966e9062
Julie <62952163+jumdc@users.noreply.github.com>
2020-12-23 19:19:33 +0100

    add config template

---
+++ config.py
@@ -0,0 +1,6 @@
+username = 'your username'
+cid = 'your cid'
+secret = 'your secret id'
+redirect_uri = 'the uri you want to rediretc to '
+uriPlaylist='uri of the playlist to update'
+idPlaylist='id of the playlist to update'
